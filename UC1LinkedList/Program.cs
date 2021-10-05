dbb19eac6a66052d226eb7cc4063b079ebd37017
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-05 18:26:45 +0530

    Hemanth : Insert 3 Integer to Linkedlist

---
+++ UC1LinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UC1LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+
+            linkedList.AddFirst(56);
+            linkedList.AddFirst(30);
+            linkedList.AddFirst(70);
+            
+
+            foreach (int i in linkedList)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
