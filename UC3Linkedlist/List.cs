fb992bd315aa5941c62bc246a8008698cbe2c1a3
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-06 18:17:30 +0530

    Hemanth : Append a New Node to existing Node

---
+++ UC3Linkedlist/List.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC3Linkedlist
+{
+    class List
+    {
+        internal Node head;
+        internal class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data+" inserted into the Linked List");
+        }
+
+        internal void Append(int Ndata)
+        {
+            Node new_node = new Node(Ndata);
+            if (head == null)
+            {
+                head = new Node(Ndata);
+                return;
+            }
+            new_node.next = null;
+            Node last = head;
+            while (last.next != null)
+                last = last.next;
+            last.next = new_node;
+            Console.WriteLine(Ndata + " append to previous node");
+            return;
+        }
+    }
+    }
+
---
+++ UC3Linkedlist/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UC3Linkedlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+
+            list.Add(56);
+            list.Append(30);
+            list.Append(70);
+        }
+    }
+}
