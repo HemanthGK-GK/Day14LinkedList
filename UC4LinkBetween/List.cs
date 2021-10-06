dfe8620b4b1540335805d061d207fb129bc72d94
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-06 18:19:18 +0530

    Hemanth : Inserting new node to specific position

---
+++ UC4LinkBetween/List.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC4LinkBetween
+{
+    class List
+    {
+        internal Node head;
+        internal class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the Linked List", node.data);
+        }
+
+        internal Node InsertNode(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Enter Valid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0)
+                {
+
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
+    }
+}
---
+++ UC4LinkBetween/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace UC4LinkBetween
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+            list.Add(56);
+            list.InsertNode(2, 30);
+            list.Add(70);
+        }
+    }
+}
