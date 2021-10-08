64901fca0e65cd7480094295eb53b29539cf282e
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-08 16:41:14 +0530

    Hemanth :Insert a Value to perticular position in linkedlist

---
+++ UC8InsertAtPosition/List.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC8InsertAtPosition
+{
+    class List
+    {
+        internal Node head;
+        public class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data + " inserted into the Linked List");
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " -> ");
+                temp = temp.next;
+            }
+        }
+
+        public void InsertAfter(Node prev_node, int data)
+        {
+            Node new_node = new Node(data);
+            new_node.next = prev_node.next;
+            prev_node.next = new_node;
+
+        }
+
+        public bool Search( int x)
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                if (temp.data == x)
+                    return true;
+                temp = temp.next;
+            }
+            return false; 
+        }
+
+    }
+}
---
+++ UC8InsertAtPosition/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UC8InsertAtPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+            if (list.Search(30) == true)
+            {
+                list.InsertAfter(list.head.next, 40);
+                Console.WriteLine("\n--------------------");
+                list.Display();
+            }
+            else
+                Console.WriteLine("Value Not Found in list");
+
+        }
+    }
+}
