979e50674f6e689aa6223cc5c69c997dcf89a450
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-08 16:31:13 +0530

    Hemanth : Delete the Node at the last

---
+++ UC6DeleteLast/List.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC6DeleteLast
+{
+    class List
+    {
+        Node head;
+        public class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data + " inserted into the Linked List");
+        }
+
+        public Node RemoveLast()
+        {
+            if (head == null)
+                return null;
+            
+            Node temp = head;
+            while (temp.next.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = null;
+            return head;
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " -> ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
---
+++ UC6DeleteLast/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UC6DeleteLast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.RemoveLast();
+            Console.WriteLine("\n------------");
+            list.Display();
+
+        }
+    }
+}
