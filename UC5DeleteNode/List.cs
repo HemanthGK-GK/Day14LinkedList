bf67419bd262af3f5f51dbe4ddaa41c94722282f
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-08 16:28:58 +0530

    Hemanth : Deleting Node from Linked List

---
+++ UC5DeleteNode/List.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC5DeleteNode
+{
+    class List
+    {
+         Node head;
+        public class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data + " inserted into the Linked List");
+        }
+
+        public Node RemoveFirst()
+        {
+            if (this.head == null)
+                return null;
+            this.head = this.head.next;
+            return this.head;
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " -> ");
+                temp = temp.next; 
+            }
+        }
+    }
+}
---
+++ UC5DeleteNode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UC5DeleteNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.RemoveFirst();
+            Console.WriteLine("\n------------");
+            list.Display();
+        }
+    }
+}
