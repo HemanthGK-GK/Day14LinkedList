ae759a236036ac43b5b723b1ec1f2bcf136e2acf
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-08 16:38:11 +0530

    Hemanth : Search a Perticular Values from Linked List

---
+++ UC7SearchKey/List.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC7SearchKey
+{
+    class List
+    {
+       internal Node head;
+        public class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data + " inserted into the Linked List");
+        }
+
+        internal void Search(int x)
+        {
+            int flag = 0;
+            Node temp = head; 
+            while (temp != null)
+            {
+                if (temp.data == x)
+                    flag = 1;  
+               
+                break;
+                temp = temp.next;
+            }
+
+            if(flag==1)
+                Console.WriteLine(x + "is present in the list");
+            else
+            Console.WriteLine(x + "is not present in the list");
+        }
+    }
+}
---
+++ UC7SearchKey/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UC7SearchKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            list.Search(51);
+        }
+    }
+}
